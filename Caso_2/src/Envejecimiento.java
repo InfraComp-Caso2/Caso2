6cafe8cf1ad16b724125ddccdef85438cf70f4e4
Alejandro Celis <ea.celis@uniandes.edu.co>
2022-10-17 21:35:23 -0500

    Envejecimiento

--- Caso_2/src/Envejecimiento.java
+++ Caso_2/src/Envejecimiento.java
@@ -1,9 +1,78 @@
-public class Envejecimiento {
-    
-    // sleep
+public class Envejecimiento
+{
 
-    // cambio de 0 de la tabla
+	private String[] contadores;
+	private ArrayList<Integer> usos;
+	private Integer cantidadPaginas;
 
-    // wait 1ms
+	public Envejecimiento(int pCantidadPaginas)
+	{
+		cantidadPaginas = pCantidadPaginas;
+		usos = new ArrayList<Integer>();
+		contadores = new String[pCantidadPaginas];
+		llenarTabla();
+		
+	}
+
+	public void llenarTabla()
+	{
+		for(int i = 0; i < cantidadPaginas; i++)
+		{
+			contadores[i] = "00000000";
+		}
+	}
+
+	public void agregarUsos(int indice)
+	{
+		usos.add(indice);
+	}
+
+	public void actualizarCiclo()
+	{
+		for(int i = 0; i < cantidadPaginas; i++)
+		{
+			boolean flag = false;
+			for(int j = 0; j < usos.size() && !flag; j++)
+			{
+				if(i == usos.get(j))
+				{
+					String edit = borrarUltimoChar(contadores[i]);
+					edit = "1" + edit;
+					contadores[i] = edit;
+					flag = true;
+				}
+				else if(j == usos.size() - 1)
+				{
+					String edit = borrarUltimoChar(contadores[i]);
+					edit = "0" + edit;
+					contadores[i] = edit;					
+				}
+			}				
+		}
+	}
+	
+	public int envejecer()
+	{
+		int lower = 128;
+		int indice = 0;
+	
+		for(int i = 0; i < cantidadPaginas; i++)
+		{
+			int comparacion = Integer.parseInt(contadores[i], 2);
+			if(comparacion < lower && comparacion != 0)
+			{
+				lower = comparacion;
+				indice = i;
+			}
+		}
+		contadores[indice] = "00000000";
+		agregarUsos(indice);
+		return indice;
+	}
+	
+	public static String borrarUltimoChar(String str)
+	{
+        return str.substring(0, str.length() - 1);
+    }
 
 }
